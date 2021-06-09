ef14a462f5a9c1799159984cadad6fae0e0fafd9
LEO <leous2013@gmail.com>
2021-06-08 21:55:56 -0700

    test

---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Admin extends Component {
+    render() {
+        return (
+            <div>
+                this is the admin component
+            </div>
+        );
+    }
+}
+
+Admin.propTypes = {};
+
+export default Admin;
