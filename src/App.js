11b48d3d1ba01032c1894c4b9a2f23e564b0674f
LEO <leous2013@gmail.com>
2021-06-08 18:19:34 -0700

    addfile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React, {Component} from 'react'
+import {Button} from 'antd'
+import 'antd/dist/antd.less'
+
 
 function App() {
   return (
     <div >
-      SPP
+      APP
+      <Button type='primary'>This is a button</Button>
     </div>
   );
 }
